29535c0aa35c1ed6b8d83043eafbe26dbd0ec9c4
srobertson421 <srobertson421@gmail.com>
2018-02-01 11:46:08 -0800

    working go cli version

--- go-version/corpus-search.go
+++ go-version/corpus-search.go
@@ -6,11 +6,15 @@ import (
   "os"
   "io/ioutil"
   "path/filepath"
+  "regexp"
+  "strings"
 )
 
 func main() {
   directoryArg := flag.String("dir", "", "Directory containing text docs")
   flag.Parse()
+
+  words := flag.Args()
 
   if *directoryArg == "" {
     flag.PrintDefaults()
@@ -27,7 +31,70 @@ func main() {
     fmt.Print(err)
   }
 
+  regex, err := regexp.Compile("[^a-zA-Z0-9 ]+")
+  if err != nil {
+    fmt.Print(err)
+  }
+
+  countMap := make(map[string]map[string]float64)
+
   for _, fileStat := range dirInfo {
-    fmt.Printf("%s\n", fileStat.Name())
+    fileName := fileStat.Name()
+    countMap[fileName] = make(map[string]float64)
+
+    for _, word := range words {
+      countMap[fileName][word] = 0
+    }
+
+    absFilePath, err := filepath.Abs(filepath.Join(*directoryArg, fileStat.Name()))
+    if err != nil {
+      fmt.Print(err)
+    }
+
+    bytes, err := ioutil.ReadFile(absFilePath)
+    if err != nil {
+      fmt.Print(err)
+    }
+
+    content := string(bytes)
+    processedContent := strings.ToLower(regex.ReplaceAllString(content, ""))
+    splitContent := strings.Fields(processedContent)
+
+    for _, content := range splitContent {
+      for _, word := range words {
+        if(word == content) {
+          countMap[fileName][word] = float64(countMap[fileName][word] + 1) / float64(len(splitContent))
+        }
+      }
+    }
+  }
+
+  resultsMap := make(map[string]map[string]float64)
+
+  for _, word := range words {
+    resultsMap[word] = make(map[string]float64)
+  }
+
+  for word, _ := range resultsMap {
+    previousScore := float64(0)
+    for fileName, termMap := range countMap {
+      tfScore := termMap[word]
+      if previousScore == float64(0) {
+        previousScore = tfScore
+        resultsMap[word][fileName] = tfScore
+      } else {
+        if tfScore > previousScore {
+          resultsMap[word] = make(map[string]float64)
+          resultsMap[word][fileName] = tfScore
+          previousScore = tfScore
+        }
+      }
+    }
+  }
+
+  for word, fileMap := range resultsMap {
+    for fileName, tfScore := range fileMap {
+      fmt.Println(word, "-", fileName, "-", tfScore)
+    }
   }
 }
